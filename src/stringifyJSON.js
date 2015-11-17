e5c968b6e2ac240cfbefa7ceaeab4172cc0073d1
Taylor Richards <taylormaliarichards@192.168.1.10>
2015-11-16 19:58:36 -0500

    solved through the array test cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,17 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof(obj) === 'number' || obj === null || typeof(obj) === 'boolean' ) {
+    obj = '' + obj;
+  } else if(typeof(obj) === 'string') {
+    obj = '"' + obj + '"';
+  } else if(Array.isArray(obj)) {
+    
+    var middle = [];
+    for(var i = 0; i < obj.length; i++) {
+      middle.push(stringifyJSON(obj[i]));
+    }
+    obj = '[' + middle.join(",") + ']';
+  } else if(typeof(obj))
+  return obj;
 }
